d061e80d5628f028ff0b79ff818b24ed082993c0
nimazmedia <81438988+nimazmedia@users.noreply.github.com>
2021-12-30 13:31:41 -0300

    correcciones Entrega Final

--- src/components/FinalCompra/FinalCompra.jsx
+++ src/components/FinalCompra/FinalCompra.jsx
@@ -6,7 +6,9 @@ const CompraFinalizada = (props) => {
     return (
         <div>
             <Modal {...props}>
-                <Modal.Body className="fs-5">Tu compra ha sido realizada con éxito. El envío será realizado próximamente. <br />
+                <Modal.Header>Tu compra ha sido realizada con éxito.</Modal.Header>
+                <Modal.Body className="fs-5">
+                    Su código de orden es el: {props.orderid}. El envío será realizado próximamente. <br />
                     Muchas gracias por comprar en F1 Shop. 
                 </Modal.Body>
                 <Modal.Footer>
