856e2972b56fc9ab184dc9d6b705b5737a1c36dc
nimazmedia <81438988+nimazmedia@users.noreply.github.com>
2021-12-30 13:35:37 -0300

    correcciones Entrega Final

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -30,24 +30,9 @@ export const Cart = () => {
             const price = cartItem.price * cartItem.cantidad
             return { id, title, price }
         })
+        
+        postOrder(order, setOrderId, cartList)
 
-        const dbQ = getFirestore()
-        const orders = dbQ.collection('orders')
-
-        orders.add(order)
-            .then((res) => { setOrderId(res.id) })
-            .catch(err => console.log(err))
-
-            const itemsToUpdate = dbQ.collection('items').where(firebase.firestore.FieldPath.documentId(), 'in', cartList.map(i => i.id))
-            const batch = dbQ.batch();
-
-        itemsToUpdate.get()
-            .then(collection => {
-                collection.docs.forEach(docSnapshot => {
-                    batch.update(docSnapshot.ref, { stock: docSnapshot.data().stock - cartList.find(item => item.id === docSnapshot.id).count })
-                })
-                batch.commit().then(res => {console.log(`Stock actualizado`)})
-            })
     }
 
     const handleHide = () => {
